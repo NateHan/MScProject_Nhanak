9157699e7923adcb55d35e49415ff75b6a5af48c
nateHan <nhanak01@dcs.bbk.uk>
2017-07-07 15:36:53 +0100

    added a login page, navigates to it when clicking register

---
+++ trackwild/app/controllers/LoginRegController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import play.filters.headers.SecurityHeadersFilter
+
+/**
+  * Created by nathanhanak on 7/7/17.
+  */
+@Singleton
+class LoginRegController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  /**
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
+  def loadLogin() = Action {
+    implicit request: Request[AnyContent] => Ok(views.html.loginreg()).withHeaders(SecurityHeadersFilter
+      .CONTENT_SECURITY_POLICY_HEADER -> " .fontawesome.com .fonts.googleapis.com")
+  }
+}
