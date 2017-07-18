2ad18fe7dfc3b408ab7022503b27ab8df3ab8ed1
nateHan <nhanak01@dcs.bbk.uk>
2017-07-18 22:51:39 +0100

    Used AJAX to dynamically fly-in content

--- trackwild/app/controllers/ProjectsWorkSpaceController.scala
+++ trackwild/app/controllers/ProjectsWorkSpaceController.scala
@@ -8,18 +8,18 @@ import play.filters.headers.SecurityHeadersFilter
 /**
   * Created by nathanhanak on 7/16/17.
   */
-class ProjectsWorkSpaceController @Inject() (cc: ControllerComponents) extends AbstractController(cc){
+class ProjectsWorkSpaceController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def loadWorkspace(projectName:String, userName:String) = Action {
-    implicit request: Request[AnyContent] => Ok(views.html.afterLogin
-      .projectworkspace.projectView(projectName, userName))
-      .withHeaders(SecurityHeadersFilter
-      .CONTENT_SECURITY_POLICY_HEADER -> " .fontawesome.com .fonts.googleapis.com")
+  def loadWorkspace(projectName: String, userName: String) = Action {
+    implicit request: Request[AnyContent] =>
+      Ok(views.html.afterLogin
+        .projectworkspace.projectView(projectName, userName))
+        .withHeaders(SecurityHeadersFilter
+          .CONTENT_SECURITY_POLICY_HEADER -> " .fontawesome.com .fonts.googleapis.com")
   }
 
   def renderNewTableUploader() = Action {
     implicit request: Request[AnyContent] => Ok(views.html.afterLogin.projectworkspace.newDataUploader())
   }
-
 
 }
