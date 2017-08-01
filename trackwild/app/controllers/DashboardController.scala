5c17f989cbf4f2c67235ce613b48c2839ddb1aa6
nateHan <nhanak01@dcs.bbk.uk>
2017-08-01 21:47:30 +0100

    Created controller method for project creation. Need to hook it up via AJAX/jQuery

--- trackwild/app/controllers/DashboardController.scala
+++ trackwild/app/controllers/DashboardController.scala
@@ -1,7 +1,9 @@
 package controllers
 
+import java.sql.SQLException
 import javax.inject.{Inject, Singleton}
 
+import models.database.DatabaseUpdate
 import models.formdata.NewProjectData
 import play.api.data.Form
 import play.api.data.Forms._
@@ -16,11 +18,13 @@ class DashboardController @Inject()(twDB: Database, authController: Authenticati
 
   /**
     * The first page to load after a user logs in
+    *
     * @return a Result containing the dashboard home page
     */
   def index() = Action {
-    implicit request: Request[AnyContent] => authController.returnDesiredPageIfAuthenticated(
-      request, views.html.afterLogin.dashboardviews.dashboard(), "Login failed")
+    implicit request: Request[AnyContent] =>
+      authController.returnDesiredPageIfAuthenticated(
+        request, views.html.afterLogin.dashboardviews.dashboard(), "Login failed")
   }
 
   /**
@@ -28,21 +32,23 @@ class DashboardController @Inject()(twDB: Database, authController: Authenticati
     * The projects they lead
     * The projects they are invited to collaborate on
     * Or create a new project
+    *
     * @return
     */
   def loadUserProjects() = Action {
-    implicit request: Request[AnyContent] => authController.returnDesiredPageIfAuthenticated(
-      request, views.html.afterLogin.dashboardviews.userProjects(), "Credentials Expired")
+    implicit request: Request[AnyContent] =>
+      authController.returnDesiredPageIfAuthenticated(
+        request, views.html.afterLogin.dashboardviews.userProjects(), "Credentials Expired")
   }
 
-  def projectOptionPicker(page:String) = Action {
+  def projectOptionPicker(page: String) = Action {
     implicit request: Request[AnyContent] =>
       val pageToLoad = page match {
         case "userLead" => views.html.afterLogin.dashboardviews.userLeadProjects()
         case "userCollab" => views.html.afterLogin.dashboardviews.userCollabProjects()
         case "newProject" => views.html.afterLogin.dashboardviews.newProjectCreator()
       }
-    authController.returnDesiredPageIfAuthenticated(request, pageToLoad, "Credentials Expired")
+      authController.returnDesiredPageIfAuthenticated(request, pageToLoad, "Credentials Expired")
   }
 
   val newProjForm: Form[NewProjectData] = Form {
@@ -53,14 +59,36 @@ class DashboardController @Inject()(twDB: Database, authController: Authenticati
     )(NewProjectData.apply)(NewProjectData.unapply)
   }
 
+  /**
+    * Method responsible for the creation of a new project from the dashboard.
+    * From the user form, it creates a new project within all_projects DB table and then
+    * inserts a new note in the project_notes.
+    * @return an Ok response if successful, a BadRequest if there is a failure.
+    */
   def postNewProject() = Action {
-    implicit request: Request[AnyContent] => newProjForm.bindFromRequest().fold(
-      errorForm => BadRequest("Project Creation Failed"),
-      successForm => {
-
-        Ok("Project Created")}
-    )
+    implicit request: Request[AnyContent] =>
+      newProjForm.bindFromRequest().fold(
+        errorForm => BadRequest("Project Creation Failed"),
+        successForm => {
+          val colsToValsProjects: Map[String, String] = Map("project_title" -> successForm.title,
+            "project_lead" -> successForm.userName)
+          val colsToValsNotes: Map[String, String] = Map("project_title" -> successForm.title,
+            "note_title" -> s"${successForm.userName} created project.",
+            "note_author" -> successForm.userName,
+            "note_content" -> successForm.initialNote
+          )
+          try {
+            val rowsInsertedProjects = DatabaseUpdate.insertInto("all_projects", colsToValsProjects)
+            val rowsInsertedNotes = DatabaseUpdate.insertInto("project_notes", colsToValsNotes)
+            if (rowsInsertedProjects == 1 && rowsInsertedNotes == 1) {
+              Ok("Project Created")
+            }
+            else (BadRequest("Project Creation Failed"))
+          } catch {
+            case e: SQLException => e.printStackTrace; BadRequest("Project Creation Failed - SQL Error")
+            case unknown => unknown.printStackTrace; BadRequest("Project Creation Failed - SQL Error")
+          }
+        }
+      )
   }
-
-
 }
--- trackwild/app/models/database/DatabaseUpdate.scala
+++ trackwild/app/models/database/DatabaseUpdate.scala
@@ -1,6 +1,6 @@
 package models.database
 
-import java.sql.{DatabaseMetaData, ResultSet, Statement}
+import java.sql.{ResultSet, Statement}
 
 import models.database.DefaultDataBase
 import play.api.db.Database
@@ -8,21 +8,20 @@ import play.api.db.Database
 object DatabaseUpdate {
 
   /**
-    * Method which executes an SQL INSERT statement into the database. Checks the column
-    * types of the table and will automatically add ' ' around values which need them for legal
-    * SQL insert. TEST THIS
-    * @param tableName The name of the table on which to perform the insert.
+    * Method which executes an SQL INSERT statement into the database.
+    * Will automatically add ' ' around all input values to insure they are legal SQL
+    *
+    * @param tableName     The name of the table on which to perform the insert.
     * @param columnsToVals A map of the table's ( columns -> valuesToinsert).
-    * @param expectedRows How many rows are expected to be affected by the insert, used for validation
     * @return Int the number of rows which were affected
     */
-  def insertInto(tableName:String, columnsToVals:Map[String,String]): Int = {
+  def insertInto(tableName: String, columnsToVals: Map[String, String]): Int = {
     val db: Database = DefaultDataBase.getApplicationDataBase
     val columnBuilder = new StringBuilder
     val valueBuilder = new StringBuilder
-    db.withConnection{ conn =>
+    db.withConnection { conn =>
       val stmt = conn.createStatement()
-      for( (k,v)  <- columnsToVals) {
+      for ((k, v) <- columnsToVals) {
         columnBuilder.append(k + ", ");
         if (v.startsWith("'") && v.endsWith("'")) {
           valueBuilder.append(v + ", ")
@@ -30,16 +29,10 @@ object DatabaseUpdate {
           valueBuilder.append("'" + v + "', ")
         }
       }
-      columnBuilder.deleteCharAt(columnBuilder.length-2) //remove erroneous commas
-      valueBuilder.deleteCharAt(valueBuilder.length-2) //remove erroneous commas
+      columnBuilder.deleteCharAt(columnBuilder.length - 2) //remove erroneous commas
+      valueBuilder.deleteCharAt(valueBuilder.length - 2) //remove erroneous commas
       stmt.executeUpdate(s"INSERT INTO $tableName(${columnBuilder.toString()}) VALUES(${valueBuilder.toString()});")
     }
   }
 
 }
-
-//  have input map columns -> vales
-//  Check column type
-//  check if column type needs ' ' s AND THEN values don't have them
-//    -Add ' '
-//    -else don't add them
