1306755c2c05ae2467e4a5a2d57663f91a0aa4ba
nateHan <nhanak01@dcs.bbk.uk>
2017-08-22 17:32:10 -0500

    Embedded Google Maps finally

--- trackwild/public/javascripts/main.js
+++ trackwild/public/javascripts/main.js
@@ -141,3 +141,18 @@ $(document).on('click', '.dataPickerIconDiv', function() {
     xhttp.send();
 
 });
+
+function initMap() {
+
+    document.getElement
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru,
+        mapTypeId: 'satellite'
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
